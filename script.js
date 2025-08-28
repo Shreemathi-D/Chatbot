8ab2881c67e445896150bbe3238452f58da13a53
Shreemathi-D <146478830+Shreemathi-D@users.noreply.github.com>
2025-08-28 22:38:40 +0530

    Update script.js - key removed

--- script.js
+++ script.js
@@ -29,7 +29,6 @@ async function loadConfig() {
     // Placeholder: Replace with fetch('config.json')
     config = await Promise.resolve({
       cogSvcRegion: "eastus2",
-      // cogSvcSubKey: "Cz4BbPc7lZ9XlsBO0qUVgqLsvmoSa1Nq4dgoxmAurG7lFgVubdyTJQQJ99BHACHYHv6XJ3w3AAAAACOGowZU",
       talkingAvatarCharacter: "lisa",
       talkingAvatarStyle: "casual-sitting",
       ttsVoice: "en-US-JennyNeural",
@@ -703,4 +702,5 @@ window.microphone = () => {
  
 window.stopSpeaking = stopSpeaking;
  
+
 
